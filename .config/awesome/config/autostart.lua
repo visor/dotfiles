6ca40820ab3bd105171a1c5cdcb20c211d6d3ce0
Victor Karamzin <admin@visor.ws>
2013-09-15 00:41:12 +0700

    xrandr fix

--- .config/awesome/config/autostart.lua
+++ .config/awesome/config/autostart.lua
@@ -1,6 +1,5 @@
 -- Настройка экранов
-run("xrandr --output DVI-I-0 --mode 2048x1152")
-run("xrandr --output DVI-I-1 --mode 1280x1024 --right-of DVI-I-0")
+run("xrandr --output DVI-I-0 --mode 2048x1152 --output DVI-I-1 --mode 1280x1024 --right-of DVI-I-0")
 
 
 run("wmname LG3D") --Для Java
