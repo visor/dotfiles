0445ce2d2bed26f6caee97f70327f4bd63662df3
Victor Karamzin <admin@visor.ws>
2013-09-15 00:21:03 +0700

    More comments

--- .config/awesome/config/autostart.lua
+++ .config/awesome/config/autostart.lua
@@ -4,17 +4,18 @@ run("xrandr --output DVI-I-1 --mode 1400x1050 --right-of DVI-I-0")
 
 
 run("wmname LG3D") --Для Java
-run("setxkbmap -layout 'us,ru' -model pc105 -variant nodeadkeys,intl") -- Настройки раскладок клавиатуры
+
+
+run("setxkbmap -layout 'us,ru(winkeys)'") -- Настройки раскладок клавиатуры
 run("setxkbmap -option")
-run("setxkbmap -option 'grp:shift_toggle'")
-run("setxkbmap -option 'misc:typo,lv3:ralt_switch,compose:menu'")
-run("setxkbmap -option 'terminate:ctrl_alt_bksp'")
+run("setxkbmap -option 'grp:shift_toggle'") -- Два шифта переключение расклдаки
+run("setxkbmap -option 'misc:typo,lv3:ralt_switch,compose:menu'") -- Типография и спецсимволы
+run("setxkbmap -option 'terminate:ctrl_alt_bksp'") -- рестарт иксов
 
 
 run_once("xscreensaver", "-no-splash") -- Скринсейвер
 run_once("nm-applet", "--sm-disable") -- Монитор сетевых подключений
 run_once("parcellite") -- Монитор буфера обмена
---run_once("mpd") -- Музыка
 --todo: cmus
 
 
