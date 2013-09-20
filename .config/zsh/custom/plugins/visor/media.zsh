3966efac97cdbf1a3f8656fac31daeedb6ff5f85
Victor Karamzin <admin@visor.ws>
2013-09-21 01:40:30 +0700

    add livestreamer alias

--- .config/zsh/custom/plugins/visor/media.zsh
+++ .config/zsh/custom/plugins/visor/media.zsh
@@ -1,8 +1,11 @@
 function podcast() {
-	nvlc --no-color -S podcast --podcast-urls "$@";
+	nvlc --no-color -S podcast --podcast-urls "$@"
+}
+
+function live() {
+	livestreamer -p 'mplayer -vo x11' "$1" best
 }
 
 alias mplayer="mplayer -zoom -fs"
 alias M='mplayer -vo x11 -fs -zoom'
-
 alias img="feh -xdF"
