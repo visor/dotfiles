b1e7b19342d4c4739c304e4203f7142c9bc6fa5b
Victor Karamzin <admin@visor.ws>
2013-09-21 01:13:24 +0700

    Clean

--- .config/zsh/custom/plugins/visor/profile.zsh
+++ .config/zsh/custom/plugins/visor/profile.zsh
@@ -5,8 +5,6 @@ export TERM=rxvt
 export COLORTERM=rxvt
 export WINDOW_MANAGER=xmonad
 
-export PATH
-
 export JAVA_HOME=/usr/java/latest
 export PATH=$PATH:$JAVA_HOME/bin:/var/www/hosts/php-tools/bin:$HOME/bin
 
